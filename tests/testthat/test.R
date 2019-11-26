312460f45bd2603cdd203c710eb6810e33debdb3
Xuelin Gu <xuelingu@umich.edu>
2019-11-26 10:37:36 -0500

    add bench install

--- tests/testthat/test.R
+++ tests/testthat/test.R
@@ -13,11 +13,6 @@ x4 = mtcars$hp
 
 ##1.1 cope with different compatible formula format
 test_that("testPkg",  {
-  y = c(23, 24, 26, 37, 38, 25, 36, 40)
-  x1 = c(1, 2, 3, 4, 5, 6, 7, 8)
-  x2 = c(23, 32, 34, 20, 24, 56, 34, 24)
-  x3 = c("M", "F", "F", "U", "M", "F", "U", "M")
-  x4 = mtcars$hp
   expect_equal(as.numeric(lr(y ~ x1)[[1]][, 1]), as.numeric(lm(y ~ x1)[[1]]))
   expect_equal(as.numeric(lr(y ~ x1 + x2)[[1]][, 1]), as.numeric(lm(y ~ x1 + x2)[[1]]))
   expect_equal(as.numeric(lr(mtcars$mpg ~ mtcars$cyl + mtcars$disp)[[1]][, 1]), as.numeric(lm(mtcars$mpg ~ mtcars$cyl + mtcars$disp)[[1]]))
